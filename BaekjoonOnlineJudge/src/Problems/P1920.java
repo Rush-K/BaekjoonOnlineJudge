f1c45d9e875da5ea4d9e1176c1a8a0ef5447805f
Rush K <k_rush@naver.com>
2021-07-29 12:11:04 +0900

    P1920(수 찾기) 문제 해결 - HashMap 이용

--- BaekjoonOnlineJudge/src/Problems/P1920.java
+++ BaekjoonOnlineJudge/src/Problems/P1920.java
@@ -1,11 +1,17 @@
+/** Baekjoon Online Judge
+*   Problem No. 1920
+*   수 찾기 
+*   Writed by Rush.K
+*   Using HashMap 
+*/
+
 package Problems;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class P1920 {
-	public static int[] solution(int[] numbers, int[] findNumbers) {
+	public static int[] solution(int[] numbers, int[] findNumbers) { // HashMap에 수 저장 후 탐색 
 		HashMap<Integer, Integer> hashMap = new HashMap<>();
 		int[] answer = new int[findNumbers.length];
 		
@@ -31,5 +37,4 @@ public class P1920 {
 		int[] answer = solution(numbers, findNumbers);
 		for (int i = 0; i < answer.length; i++) System.out.println(answer[i]);
 	}
-
 }
