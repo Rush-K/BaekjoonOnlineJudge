ed8983c171c07a490e3cc2690cb93f31347d3fa2
Rush K <k_rush@naver.com>
2021-08-26 17:23:06 +0900

    P9376(탈옥) 문제 해결중...

--- BaekjoonOnlineJudge/src/Problems/P9376.java
+++ BaekjoonOnlineJudge/src/Problems/P9376.java
@@ -45,48 +45,114 @@ public class P9376 {
     public static int h;
     public static int w;
     public static char[][] map;
-    public static boolean[][] visited;
-    public static int openedDoors;
+    public static int[][] visited;
 
-    public static void prisonerMoving(LocationInPrison prisoner) {
+    public static int prisonerMoving(LocationInPrison firstPrisoner, LocationInPrison secondPrisoner) {
         PriorityQueue<LocationInPrison> q = new PriorityQueue<>(new Comparator<LocationInPrison>() {
             @Override
             public int compare(LocationInPrison o1, LocationInPrison o2) {
-                return map[o1.x][o1.y] == '.' ? -1 : 1;
+                if (o1.doors.size() == o2.doors.size()) {
+                    return map[o1.x][o1.y] == '.' ? -1 : 1;
+                }
+                return o1.doors.size() > o2.doors.size() ? 1 : -1;
             }
         });
 
+        ArrayList<LocationInPrison> pathList = new ArrayList<>();
         int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        q.add(prisoner);
-        visited[prisoner.x][prisoner.y] = true;
+        int firstOpenedDoors = Integer.MAX_VALUE;
+
+        visited = new int[h][w];
+        q.add(firstPrisoner);
+        visited[firstPrisoner.x][firstPrisoner.y] = 1;
 
         while (!q.isEmpty()) {
             LocationInPrison lip = q.poll();
             if (lip.x == 0 || lip.x == h - 1 || lip.y == 0 || lip.y == w - 1) {
-                openedDoors += lip.doors.size();
-                for (Door l : lip.doors) map[l.x][l.y] = '.';
-                return;
+                pathList.add(lip);
+                continue;
             }
 
             for (int i = 0; i < directions.length; i++) {
                 try {
-                    if (map[lip.x - directions[i][0]][lip.y - directions[i][1]] != '*' && visited[lip.x - directions[i][0]][lip.y - directions[i][1]] == false) {
+                    if (map[lip.x - directions[i][0]][lip.y - directions[i][1]] != '*' && visited[lip.x - directions[i][0]][lip.y - directions[i][1]] == 0) {
                         if (map[lip.x - directions[i][0]][lip.y - directions[i][1]] == '#') {
-                            visited[lip.x - directions[i][0]][lip.y - directions[i][1]] = true;
+                            visited[lip.x - directions[i][0]][lip.y - directions[i][1]] = 1;
                             q.add(new LocationInPrison(lip.x - directions[i][0], lip.y - directions[i][1], lip.doors, new Door(lip.x - directions[i][0], lip.y - directions[i][1])));
 
                         } else {
-                            visited[lip.x - directions[i][0]][lip.y - directions[i][1]] = true;
+                            visited[lip.x - directions[i][0]][lip.y - directions[i][1]] = 1;
                             q.add(new LocationInPrison(lip.x - directions[i][0], lip.y - directions[i][1], lip.doors));
                         }
                     }
-                } catch (Exception e) {}
+                } catch (Exception e) { System.out.println(e); }
             }
         }
+
+        for (LocationInPrison temp : pathList) {
+            visited = new int[h][w];
+            for (Door l : temp.doors) {
+                visited[l.x][l.y] = 2;
+                map[l.x][l.y] = '.';
+            }
+            int secondOpenedDoors = secondPrisonerMoving(secondPrisoner);
+            firstOpenedDoors = Math.min(firstOpenedDoors, temp.doors.size() + secondOpenedDoors);
+
+            for (Door l : temp.doors) map[l.x][l.y] = '#';
+        }
+
+        return firstOpenedDoors;
+    }
+
+    public static int secondPrisonerMoving(LocationInPrison prisoner) {
+        PriorityQueue<LocationInPrison> q = new PriorityQueue<>(new Comparator<LocationInPrison>() {
+            @Override
+            public int compare(LocationInPrison o1, LocationInPrison o2) {
+                if (o1.doors.size() == o2.doors.size()) {
+                    return map[o1.x][o1.y] == '.' ? -1 : 1;
+                }
+                return o1.doors.size() > o2.doors.size() ? 1 : -1;
+            }
+        });
+
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int secondOpenedDoors = Integer.MAX_VALUE;
+
+        q.add(prisoner);
+        visited[prisoner.x][prisoner.y] = 1;
+
+        while (!q.isEmpty()) {
+            LocationInPrison lip = q.poll();
+            if (lip.x == 0 || lip.x == h - 1 || lip.y == 0 || lip.y == w - 1 || visited[lip.x][lip.y] == 2) {
+                secondOpenedDoors = Math.min(secondOpenedDoors, lip.doors.size());
+                continue;
+            }
+
+            for (int i = 0; i < directions.length; i++) {
+                try {
+                    if (map[lip.x - directions[i][0]][lip.y - directions[i][1]] != '*' && visited[lip.x - directions[i][0]][lip.y - directions[i][1]] != 1) {
+                        if (map[lip.x - directions[i][0]][lip.y - directions[i][1]] == '#') {
+                            visited[lip.x - directions[i][0]][lip.y - directions[i][1]] = 1;
+                            q.add(new LocationInPrison(lip.x - directions[i][0], lip.y - directions[i][1], lip.doors, new Door(lip.x - directions[i][0], lip.y - directions[i][1])));
+                        } else {
+                            if (visited[lip.x - directions[i][0]][lip.y - directions[i][1]] == 2) {
+                                q.add(new LocationInPrison(lip.x - directions[i][0], lip.y - directions[i][1], lip.doors));
+                            } else {
+                                visited[lip.x - directions[i][0]][lip.y - directions[i][1]] = 1;
+                                q.add(new LocationInPrison(lip.x - directions[i][0], lip.y - directions[i][1], lip.doors));
+                            }
+                         }
+                    }
+                } catch (Exception e) { }
+            }
+        }
+
+        return secondOpenedDoors;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int[][] prisoners = new int[2][2];
         int T = Integer.parseInt(br.readLine());
 
@@ -96,6 +162,7 @@ public class P9376 {
             w = Integer.parseInt(input[1]);
             map = new char[h][w];
             int prisonerNumber = 0;
+            int answer = 0;
 
             for (int j = 0; j < h; j++) {
                 char[] mapInput = br.readLine().toCharArray();
@@ -110,29 +177,13 @@ public class P9376 {
                     }
                 }
             }
-            visited = new boolean[h][w];
-            openedDoors = 0;
-            prisonerMoving(new LocationInPrison(prisoners[0][0], prisoners[0][1]));
-            for (int j = 0; j < h; j++) {
-                for (int k = 0; k < w; k++) {
-                    System.out.print(map[j][k] + " ");
-                }
-                System.out.println();
-            }
-            System.out.println();
+
+            answer = Math.min(prisonerMoving(new LocationInPrison(prisoners[0][0], prisoners[0][1]), new LocationInPrison(prisoners[1][0], prisoners[1][1])),
+                    prisonerMoving(new LocationInPrison(prisoners[1][0], prisoners[1][1]), new LocationInPrison(prisoners[0][0], prisoners[0][1])));
 
 
-            visited = new boolean[h][w];
-            prisonerMoving(new LocationInPrison(prisoners[1][0], prisoners[1][1]));
-            for (int j = 0; j < h; j++) {
-                for (int k = 0; k < w; k++) {
-                    System.out.print(map[j][k] + " ");
-                }
-                System.out.println();
-            }
-
-
-            System.out.println(openedDoors);
+            sb.append(answer + "\n");
         }
+        System.out.print(sb);
     }
 }
